801be57f846c2a5ccd15eddf2e2dafde82405d25
Tony Bowden <tony@tmtm.com>
2015-08-01 07:34:34 +0100

    less verbosity

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,5 @@ require 'wikidata/fetcher'
 
 WikiData::Category.new('Catégorie:Député de la XIVe législature de la Ve République', 'fr').wikidata_ids.each do |id|
   data = WikiData::Fetcher.new(id: id).data('fr','en') or next
-  puts '%s %s' % [data[:id], data[:name]]
   ScraperWiki.save_sqlite([:id], data)
 end
