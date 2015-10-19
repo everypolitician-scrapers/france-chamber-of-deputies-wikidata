858a6cad8d7681c4537c96f0675e15c820a0b2e3
Tony Bowden <tony@tmtm.com>
2015-10-19 20:54:50 +0100

    Fetch list from scraper

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,31 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'json'
+require 'pry'
+require 'rest-client'
 require 'scraperwiki'
 require 'wikidata/fetcher'
+require 'mediawiki_api'
 
-WikiData::Category.new('Catégorie:Député de la XIVe législature de la Ve République', 'fr').wikidata_ids.each_with_index do |id, i|
-  puts i if (i % 50).zero?
-  data = WikiData::Fetcher.new(id: id).data('fr','en') or next
+def members
+  morph_api_url = 'https://api.morph.io/tmtmtmtm/france-national-assembly-wikipedia/data.json'
+  morph_api_key = ENV["MORPH_API_KEY"]
+  result = RestClient.get morph_api_url, params: {
+    key: morph_api_key,
+    query: "select DISTINCT(wikiname) AS wikiname from data"
+  }
+  JSON.parse(result, symbolize_names: true)
+end
+
+WikiData.ids_from_pages('fr', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
+  puts i if (i % 30).zero?
+  data = WikiData::Fetcher.new(id: p.last).data('fr') rescue nil
+  unless data
+    warn "No data for #{p}"
+    next
+  end
+  data[:original_wikiname] = p.first
   ScraperWiki.save_sqlite([:id], data)
 end
+
