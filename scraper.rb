1de91717ad4496d9cd156d1ef3bc5fbd6273276a
Tony Bowden <tony@tmtm.com>
2016-05-22 14:46:22 +0100

    Get as Category

--- scraper.rb
+++ scraper.rb
@@ -9,10 +9,13 @@ require 'wikidata/fetcher'
 # end
 #
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://fr.wikipedia.org/wiki/Liste_des_députés_de_la_XIVe_législature_de_la_Cinquième_République',
-  after: '//span[@id="Liste_des_d.C3.A9put.C3.A9s"]',
-  before: '//span[@id="Notes_et_r.C3.A9f.C3.A9rences"]',
-  xpath: '//table//tr//td[1]//a[not(@class="new")]/@title',
-) 
+# names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  # url: 'https://fr.wikipedia.org/wiki/Liste_des_députés_de_la_XIVe_législature_de_la_Cinquième_République',
+  # after: '//span[@id="Liste_des_d.C3.A9put.C3.A9s"]',
+  # before: '//span[@id="Notes_et_r.C3.A9f.C3.A9rences"]',
+  # xpath: '//table//tr//td[1]//a[not(@class="new")]/@title',
+# ) 
+
+names = WikiData::Category.new( 'Catégorie:Député de la XIVe législature de la Ve République', 'fr').member_titles
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names }, batch_size: 100)
