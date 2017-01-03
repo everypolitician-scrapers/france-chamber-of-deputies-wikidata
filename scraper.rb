2ba1c5e99c1335f1c87cf6967e89f05b3ce4350c
Tony Bowden <tony@tmtm.com>
2017-01-03 07:45:59 +0000

    Output how many records being fetched

--- scraper.rb
+++ scraper.rb
@@ -3,21 +3,9 @@
 
 require 'wikidata/fetcher'
 
-# names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/france-national-assembly-wikipedia', column: 'wikiname')
-# names.shuffle.each_slice(50) do |sliced|
-  # EveryPolitician::Wikidata.scrape_wikidata(names: { fr: sliced })
-# end
-#
-
-# names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  # url: 'https://fr.wikipedia.org/wiki/Liste_des_députés_de_la_XIVe_législature_de_la_Cinquième_République',
-  # after: '//span[@id="Liste_des_d.C3.A9put.C3.A9s"]',
-  # before: '//span[@id="Notes_et_r.C3.A9f.C3.A9rences"]',
-  # xpath: '//table//tr//td[1]//a[not(@class="new")]/@title',
-# ) 
-
 names = WikiData::Category.new( 'Catégorie:Député de la XIVe législature de la Ve République', 'fr').member_titles |
         WikiData::Category.new( 'Catégorie:Député de la XIIIe législature de la Ve République', 'fr').member_titles |
         WikiData::Category.new( 'Catégorie:Député de la XIIe législature de la Ve République', 'fr').member_titles
+puts "Fetching #{names.count}"
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
