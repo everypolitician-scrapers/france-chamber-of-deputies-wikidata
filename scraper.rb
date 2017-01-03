e101e6064a4ef7494337cf1701f8151d105eafad
Tony Bowden <tony@tmtm.com>
2017-01-03 07:50:48 +0000

    Don't fetch data
    Testing to see what's causing a Morph error

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,12 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new( 'Catégorie:Député de la XIVe législature de la Ve République', 'fr').member_titles |
-        WikiData::Category.new( 'Catégorie:Député de la XIIIe législature de la Ve République', 'fr').member_titles |
-        WikiData::Category.new( 'Catégorie:Député de la XIIe législature de la Ve République', 'fr').member_titles
-puts "Fetching #{names.count}"
+names = {}
+names['14'] = WikiData::Category.new( 'Catégorie:Député de la XIVe législature de la Ve République', 'fr').member_titles
+names['13'] = WikiData::Category.new( 'Catégorie:Député de la XIIIe législature de la Ve République', 'fr').member_titles
+names['12'] = WikiData::Category.new( 'Catégorie:Député de la XIIe législature de la Ve République', 'fr').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
+wanted = names.values.inject(:|)
+puts "Fetching #{wanted.count}"
+
+# EveryPolitician::Wikidata.scrape_wikidata(names: { fr: wanted })
