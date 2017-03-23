2956acbb81ec04e24aa2608d3751feb662da6a28
Tony Bowden <tony@tmtm.com>
2017-03-23 13:04:12 +0000

    Also fetch members via SPARQL
    Get everyone with a P39 membership starting after 2002-06-19

--- scraper.rb
+++ scraper.rb
@@ -9,6 +9,19 @@ names['13'] = WikiData::Category.new( 'Catégorie:Député de la XIIIe législat
 names['12'] = WikiData::Category.new( 'Catégorie:Député de la XIIe législature de la Ve République', 'fr').member_titles
 
 wanted = names.values.inject(:|)
-puts "Fetching #{wanted.count}"
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { fr: wanted }, batch_size: 250)
+# Find all Memberships since the start of the 12th Legislature
+# This will ignore anyone who has a continuous membership from before
+# that, but at the minute it's just a fallback for people missing from
+# the Categories above, so should be Good Enough For Now
+sparq = <<EOS
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 wd:Q3044918 ;
+                        pq:P580 ?start_date .
+    FILTER (?start_date >= "2002-06-19T00:00:00Z"^^xsd:dateTime) .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { fr: wanted })
